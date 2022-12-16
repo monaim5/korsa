6910b0d30ddc0eebb5638ebed871f37b832fdd0a
abdelmonaim <abdelmonaim.serrhini@gmail.com>
2022-12-16 23:19:55 +0100

    First commit

---
+++ src/test/java/com/suka/korsa/KorsaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suka.korsa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KorsaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
